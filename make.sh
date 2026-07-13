d6796eb4ee0e69db28343954f020a518e3cede6b
BaSO4X <69387481+BaSO4X@users.noreply.github.com>
2026-07-13 17:47:53 +0800

    Revert "Fix: remove duplicated GITHUB_WORKSPACE paths in make.sh"
    This reverts commit 871ff5a0dda62a06e2d5010ed5115a347e8218cd.

--- make.sh
+++ make.sh
@@ -2,16 +2,17 @@
 
 URL="$1"              # ROM 包下载地址
 GITHUB_ENV="$2"       # 输出环境变量
+GITHUB_WORKSPACE="$3" # 工作目录
 
 Red='\033[1;31m'    # 粗体红色
 Yellow='\033[1;33m' # 粗体黄色
 Blue='\033[1;34m'   # 粗体蓝色
 Green='\033[1;32m'  # 粗体绿色
 
-payload_extract=./tools/payload_extract
+payload_extract="$GITHUB_WORKSPACE"/tools/payload_extract
 
-mkdir -p ./tools
-chmod -R 755 ./tools
+mkdir -p "$GITHUB_WORKSPACE"/tools
+chmod -R 755 "$GITHUB_WORKSPACE"/tools
 
 # ROM zip 名称
 rom_zip_name=$(echo "$URL" | awk -F'/' '{print $NF}' | awk -F'?' '{print $1}')
@@ -22,13 +23,13 @@ model=$(echo "$rom_zip_name" | cut -d'-' -f1 | head -n 1 | tr -d '\r')
 
 # 直接从 URL 提取 mi_ext 分区（无需下载完整 ROM）
 echo -e "${Red}- 开始提取 mi_ext 分区"
-mkdir -p ./images
-$payload_extract -s -o ./images/ -i "${URL}" -X mi_ext -T0
+mkdir -p "$GITHUB_WORKSPACE"/images
+$payload_extract -s -o "$GITHUB_WORKSPACE"/images/ -i "${URL}" -X mi_ext -T0
 echo -e "${Green}- 提取完成"
 
 # 读取增量版本号
 echo -e "${Red}- 开始读取增量版本号"
-mi_ext_build_prop=./images/mi_ext/etc/build.prop
+mi_ext_build_prop=$GITHUB_WORKSPACE/images/mi_ext/etc/build.prop
 incremental_version=$(grep "ro.mi.xms.version.incremental=" "$mi_ext_build_prop" | awk -F "=" '{print $2}' | head -n 1 | tr -d '\r')
 echo -e "${Blue}- 机型代号: $model"
 echo -e "${Blue}- 增量版本号: $incremental_version"
